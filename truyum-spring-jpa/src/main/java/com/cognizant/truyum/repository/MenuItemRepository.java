8b71b3835d1feb816823cc7ff82392612d962b59
Sai Madhur <46928649+msaimadhur@users.noreply.github.com>
2021-03-22 23:30:44 +0530

    Update MenuItemRepository.java

--- truyum-spring-jpa/src/main/java/com/cognizant/truyum/repository/MenuItemRepository.java
+++ truyum-spring-jpa/src/main/java/com/cognizant/truyum/repository/MenuItemRepository.java
@@ -1,14 +1,13 @@
 package com.cognizant.truyum.repository;
 
 import java.util.List;
-
+import java.util.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.cognizant.truyum.model.MenuItem;
 
 public interface MenuItemRepository extends JpaRepository<MenuItem, Integer> {
 
-	@Query(value="select * from menu_item where me_date_of_launch <=CURRENT_DATE()",nativeQuery = true)
-	List<MenuItem> findByActiveItems();
+	public List<MenuItem> findByActiveAndDateOfLaunchBefore(boolean active, Date date);
+
 }
