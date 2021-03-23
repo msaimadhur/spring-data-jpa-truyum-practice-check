d037c595ae5d210f072ace8369e225af671da582
Sai Madhur <46928649+msaimadhur@users.noreply.github.com>
2021-03-23 10:07:37 +0530

    Update DateUtil.java

--- truyum-spring-jpa/src/main/java/com/cognizant/truyum/util/DateUtil.java
+++ truyum-spring-jpa/src/main/java/com/cognizant/truyum/util/DateUtil.java
@@ -2,9 +2,12 @@ package com.cognizant.truyum.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DateUtil {
-	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DateUtil.class);
+
 public static Date convertToDate(String date)
 	{
 		try {
@@ -13,7 +16,7 @@ public static Date convertToDate(String date)
 		}
 		catch(Exception e)
 		{
-			System.out.println(e.getMessage());
+			LOGGER.error(e.getMessage());
 		}
 		return null;
 	}
