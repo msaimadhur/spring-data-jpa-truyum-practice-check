1c6477924ca79d7495940fcffc80e23d4130c950
Sai Madhur <46928649+msaimadhur@users.noreply.github.com>
2021-03-23 10:08:05 +0530

    Update MenuItem.java

--- truyum-spring-jpa/src/main/java/com/cognizant/truyum/model/MenuItem.java
+++ truyum-spring-jpa/src/main/java/com/cognizant/truyum/model/MenuItem.java
@@ -102,7 +102,7 @@ public class MenuItem {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + (id ^ (id >>> 32));
 		return result;
 	}
 
